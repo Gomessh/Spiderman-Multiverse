a0ddc410548577a8eb96812adcccb332cf9e05ef
Hevi <fabiohenrick08@gmail.com>
2025-02-18 02:52:13 -0300

    last commit and last changes - carousel added

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -8,14 +8,36 @@ function handleMouseLeave() {
     document.body.id = '';
 }
 
-function addEventListnersToCard () {
+function addEventListnersToCard() {
     const cardElemments = document.getElementsByClassName("card");
 
     for (let i = 0; i < cardElemments.length; i++) {
-        const card = cardElemments[i];  
+        const card = cardElemments[i];
         card.addEventListener('mouseenter', handleMouseEnter);
         card.addEventListener('mouseleave', handleMouseLeave);
     }
 }
 
 document.addEventListener("DOMContentLoaded", addEventListnersToCard, false);
+
+function selectCarouselItem(selectedButtonElement) {
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector('.cards-carousel');
+    const transform = carousel.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector('.buttonActive');
+
+    if (activeButtonElement) {
+        activeButtonElement.classList.remove('buttonActive');
+    }
+
+    // Adiciona a nova classe sem remover a classe original
+    selectedButtonElement.classList.add('buttonActive');
+
+
+}
